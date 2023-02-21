fedb12a9c803d927b54bcc42832e5109b2d780a7
rubusta <n.a.rubusta3@gmail.com>
2023-02-21 11:28:09 +0100

    created entity user

---
+++ src/main/java/com/example/java_test_junior_developer/entity/User.java
@@ -0,0 +1,65 @@
+package com.example.java_test_junior_developer.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        private int id;
+
+        @Column(name = "name")
+        private String name;
+
+        @Column(name = "surname")
+        private String surname;
+
+        @Column(name = "bals")
+        private int bals;
+
+    public User() {
+    }
+
+    public User(int id, String name, String surname, String department, int salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.bals = bals;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getBals() {
+        return bals;
+    }
+
+    public void setBals(int bals) {
+        this.bals = bals;
+    }
+}
