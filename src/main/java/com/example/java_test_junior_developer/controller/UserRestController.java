83b5daaa1bbc5b61f1cb18235d2ee0224c7a42a7
rubusta <n.a.rubusta3@gmail.com>
2023-02-21 11:57:07 +0100

    added methods

--- src/main/java/com/example/java_test_junior_developer/controller/UserRestController.java
+++ src/main/java/com/example/java_test_junior_developer/controller/UserRestController.java
@@ -1,14 +1,39 @@
 package com.example.java_test_junior_developer.controller;
 
+import com.example.java_test_junior_developer.entity.User;
+import com.example.java_test_junior_developer.repository.UserRepository;
 import com.example.java_test_junior_developer.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> showAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return users;
+    }
+    @PostMapping("/users")
+    public User addNewUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return user;
+    }
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user) {
+        userService.saveUser(user);
+        return user;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return "Employee with ID =" + id + "  was deleted";
+    }
 }
--- src/main/java/com/example/java_test_junior_developer/service/UserServiceImpl.java
+++ src/main/java/com/example/java_test_junior_developer/service/UserServiceImpl.java
@@ -1,33 +1,43 @@
 package com.example.java_test_junior_developer.service;
 
 import com.example.java_test_junior_developer.entity.User;
+import com.example.java_test_junior_developer.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
-    private UserService userService;
+    private UserRepository userRepository;
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public void saveUser(User user) {
+        userRepository.save(user);
 
     }
 
     @Override
     public User getUser(int id) {
-        return null;
+        User user = null;
+        Optional<User> optional = userRepository.findById(id);
+        if (optional.isPresent()) {
+            user = optional.get();
+        }
+        return user;
+        //TODO  to create an exception
     }
 
     @Override
     public void deleteUser(int id) {
+        userRepository.deleteById(id);
 
     }
 }
