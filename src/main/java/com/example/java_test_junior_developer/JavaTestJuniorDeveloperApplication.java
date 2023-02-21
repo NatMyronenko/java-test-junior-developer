54c56a8c0a564e816fdeaa612e9e2709903b1ae1
rubusta <n.a.rubusta3@gmail.com>
2023-02-21 11:12:32 +0100

    init

--- src/main/java/com/example/java_test_junior_developer/JavaTestJuniorDeveloperApplication.java
+++ src/main/java/com/example/java_test_junior_developer/JavaTestJuniorDeveloperApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaTestJuniorDeveloperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaTestJuniorDeveloperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestJuniorDeveloperApplication.class, args);
+    }
 
 }
