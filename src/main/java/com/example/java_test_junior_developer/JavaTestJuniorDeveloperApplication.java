bce94b4c5d7c5993a84877081ce5e55bcf3c6557
rubusta <n.a.rubusta3@gmail.com>
2023-02-21 11:12:16 +0100

    Initial commit

---
+++ src/main/java/com/example/java_test_junior_developer/JavaTestJuniorDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_test_junior_developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestJuniorDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestJuniorDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_test_junior_developer/JavaTestJuniorDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_test_junior_developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestJuniorDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
