2b1ece821baabe3d750575d291df6e806b4cde38
rubusta <n.a.rubusta3@gmail.com>
2023-02-27 19:29:14 +0100

    updated

--- src/test/java/com/example/java_test_junior_developer/JavaTestJuniorDeveloperApplicationTests.java
+++ src/test/java/com/example/java_test_junior_developer/JavaTestJuniorDeveloperApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JavaTestJuniorDeveloperApplicationTests {
 
     @Test
-    void   contextLoads() {
+    void  contextLoads() {
     }
 
 }
