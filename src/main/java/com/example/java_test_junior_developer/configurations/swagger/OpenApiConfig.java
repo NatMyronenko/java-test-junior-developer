a83cdca9da3af4e68d358459fed8e566dffe4739
rubusta <n.a.rubusta3@gmail.com>
2023-02-28 15:53:18 +0100

    added swagger config

---
+++ src/main/java/com/example/java_test_junior_developer/configurations/swagger/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.example.java_test_junior_developer.configurations.swagger;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Employee CRUD API")
+                        .description(" Spring Boot RESTful service using springdoc-openapi and OpenAPI 3.")
+                        .version("v0.0.1"));
+    }
+}
