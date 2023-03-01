9d14e60d29f8212a227d6d4288654aca92b4a54a
rubusta <n.a.rubusta3@gmail.com>
2023-03-01 15:09:06 +0100

    done

--- src/main/java/com/example/java/test/junior/developer/JavaTestJuniorDeveloperApplication.java
+++ src/main/java/com/example/java/test/junior/developer/JavaTestJuniorDeveloperApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaTestJuniorDeveloperApplication {
 
     public  static void main(String[] args) {
-        SpringApplication.run(JavaTestJuniorDeveloperApplication.class, args);
+        SpringApplication.run(JavaTestJuniorDeveloperApplication.class,args);
     }
 
 }
