e4c50f6c23476305449c9c9b8e691560e6f8b320
rubusta <n.a.rubusta3@gmail.com>
2023-03-01 21:15:30 +0100

    added tests

--- src/main/java/com/example/java/test/junior/developer/models/User.java
+++ src/main/java/com/example/java/test/junior/developer/models/User.java
@@ -8,7 +8,7 @@ import lombok.Data;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class  User {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/test/java/com/example/java/test/junior/developer/JavaTestJuniorDeveloperApplicationTests.java
+++ src/test/java/com/example/java/test/junior/developer/JavaTestJuniorDeveloperApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.java.test.junior.developer;
 
+import com.example.java.test.junior.developer.controllers.PagesController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class JavaTestJuniorDeveloperApplicationTests {
+    @Autowired
+    private PagesController myController;
 
     @Test
-    void  contextLoads() {
+    public void contextLoads() throws Exception {
+        assertThat(myController).isNotNull();
     }
 
 }
