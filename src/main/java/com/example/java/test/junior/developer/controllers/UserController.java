7d8f700fa9a8e1d217343c52e19412c65623dd5d
rubusta <n.a.rubusta3@gmail.com>
2023-03-02 16:03:11 +0100

    added swagger + deleted old controllers

--- src/main/java/com/example/java/test/junior/developer/controllers/UserController.java
+++ src/main/java/com/example/java/test/junior/developer/controllers/UserController.java
@@ -14,33 +14,33 @@ public class UserController {
     private final UserService userService;
 
     //  1 page
-    @GetMapping("/ask-details")
-    public String askUserDetails(Model model) {
-        model.addAttribute("user", new User());
-        return "ask-details";
-    }
-// 2 page
-
-    @GetMapping("/main")
-    public String api(Model model) {
-        //TODO добавить сколько раз был пройден тест
-        return "main";
-    }
-
-    @PostMapping("/user/create")
-    public String createUser(User user) {
-        userService.saveUser(user);
-        return "redirect:/";
-    }
-
-    @GetMapping("/last")
-    public String lastPage(@ModelAttribute("user") User user) {
-        return "last";
-    }
-
-    @GetMapping("/users")
-    public String showAllUsers(Model model) {
-        model.addAttribute("users", userService.listUsers());
-        return "users";
-    }
+//    @GetMapping("/ask-details")
+//    public String askUserDetails(Model model) {
+//        model.addAttribute("user", new User());
+//        return "ask-details";
+//    }
+//// 2 page
+//
+//    @GetMapping("/main")
+//    public String api(Model model) {
+//        //TODO добавить сколько раз был пройден тест
+//        return "main";
+//    }
+//
+//    @PostMapping("/user/create")
+//    public String createUser(User user) {
+//        userService.saveUser(user);
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/last")
+//    public String lastPage(@ModelAttribute("user") User user) {
+//        return "last";
+//    }
+//
+//    @GetMapping("/users")
+//    public String showAllUsers(Model model) {
+//        model.addAttribute("users", userService.listUsers());
+//        return "users";
+//    }
 }
