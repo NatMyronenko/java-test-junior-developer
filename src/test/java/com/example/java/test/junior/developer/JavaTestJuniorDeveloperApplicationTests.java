88121915b63c58e59473a1610d8f79d50a0c31ed
rubusta <n.a.rubusta3@gmail.com>
2023-03-02 17:32:51 +0100

    changed test class

--- src/test/java/com/example/java/test/junior/developer/JavaTestJuniorDeveloperApplicationTests.java
+++ src/test/java/com/example/java/test/junior/developer/JavaTestJuniorDeveloperApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.java.test.junior.developer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class JavaTestJuniorDeveloperApplicationTests {
 
