08802726a387dbc38737808cfbb3fb91a1d6a901
rubusta <n.a.rubusta3@gmail.com>
2023-03-04 22:25:49 +0100

    changed controllers

--- src/main/java/com/example/java/test/junior/developer/controllers/UserController.java
+++ src/main/java/com/example/java/test/junior/developer/controllers/UserController.java
@@ -7,40 +7,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequiredArgsConstructor
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RestController
 @RequestMapping("/java-test")
 public class UserController {
-    private final UserService userService;
+        ConcurrentHashMap<Long, User> users = new ConcurrentHashMap<>();
 
-    //  1 page
-//    @GetMapping("/ask-details")
-//    public String askUserDetails(Model model) {
-//        model.addAttribute("user", new User());
-//        return "ask-details";
-//    }
-//// 2 page
-//
-//    @GetMapping("/main")
-//    public String api(Model model) {
-//        //TODO добавить сколько раз был пройден тест
-//        return "main";
-//    }
-//
-//    @PostMapping("/user/create")
-//    public String createUser(User user) {
-//        userService.saveUser(user);
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping("/last")
-//    public String lastPage(@ModelAttribute("user") User user) {
-//        return "last";
-//    }
-//
-//    @GetMapping("/users")
-//    public String showAllUsers(Model model) {
-//        model.addAttribute("users", userService.listUsers());
-//        return "users";
-//    }
-}
+        @GetMapping("/{id}")
+        public User getUser(@PathVariable Long id) {
+            return users.get(id);
+        }
+
+        @GetMapping("/")
+        public List<User> getAllUsers() {
+            return new ArrayList<User>(users.values());
+        }
+
+        @PostMapping("/")
+        public User getUsers(@RequestBody User user) {
+            users.put(user.getId(), user);
+            return user;
+        }
+    }
+
