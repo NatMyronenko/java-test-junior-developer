2f4dd5cbaf7fbbb666cb60f5a1756acec5a592db
Andrii Varenko <andrijvarenko@gmail.com>
2023-03-05 13:12:24 +0200

    Addition: - keycloak config to application.properties file; - KeycloakAdminClient, KeycloakProperties, POST and UserService class and Request interface.

---
+++ src/main/java/com/example/java/test/junior/developer/rest/Request.java
@@ -0,0 +1,4 @@
+package com.example.java.test.junior.developer.rest;
+
+public interface Request {
+}
---
+++ src/main/java/com/example/java/test/junior/developer/services/UserService.java
@@ -0,0 +1,38 @@
+package com.example.java.test.junior.developer.services;
+
+
+import com.example.java.test.junior.developer.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class UserService  {
+
+    private List<User> users = new ArrayList<>();
+    private long ID =0;
+    {
+        users.add(new User(++ID,"Tom","Ivanov",80));
+        users.add(new User(++ID,"Ivan","Karpenko",90));
+    }
+    //получение списка всех вопросов
+    public List<User> listUsers(){
+        return users ;
+    }
+    public void saveUser(User user){
+        user.setId(++ID);
+        users.add(user);
+    }
+//    public void deleteUsers(Long id){
+//        users.removeIf(question -> question.getId().equals(id));
+//    }
+//
+//    public User getUserById(Long id) {
+//        for (User users : users) {
+//            if (users.getId().equals(id)) return users;
+//        }
+//        return null;
+ //   }
+}
