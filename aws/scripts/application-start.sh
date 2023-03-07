be4857139b0a1db16071d4050cd01b6cc903fad4
Oleksandr Pohorelov <sas.pogorelov@gmail.com>
2023-03-07 21:07:42 +0400

    Fix start sh script

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -4,7 +4,7 @@
 cd /home/ec2-user/app
 
 # Start the Java process in the background
-nohup java -jar java-test-junior-developer.jar > /dev/null 2>&1 &
+nohup java -jar java-test-junior-developer-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 
 # Wait for the Java process to start up
 sleep 10
