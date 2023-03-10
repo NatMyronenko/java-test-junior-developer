60eab78f35654f71726f3a1e766273d238cecfae
YazheV1ka <fuksvik@gmail.com>
2023-03-10 12:33:50 +0200

    Expose CRUD endpoints for language entity #3

--- src/main/java/com/example/java/test/junior/developer/controller/LanguageController.java
+++ src/main/java/com/example/java/test/junior/developer/controller/LanguageController.java
@@ -2,7 +2,6 @@ package com.example.java.test.junior.developer.controller;
 
 import com.example.java.test.junior.developer.model.Language;
 import com.example.java.test.junior.developer.service.LanguageService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,23 +18,25 @@ public class LanguageController {
         this.languageService = languageService;
     }
 
-    @PostMapping
+    @PostMapping("/save")
     public Language createLanguage(@RequestBody Language language) {
         return languageService.createLanguage(language);
     }
 
-    @GetMapping
+    @GetMapping("/get")
     public List<Language> getLanguages() {
         return languageService.getAllLanguages();
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public Language updateLanguage(Long id, Language language) {
         return languageService.updateLanguage(id, language);
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteLanguage(Long id) {
-        deleteLanguage(id);
+    @DeleteMapping("/delete/{id}")
+    public void deleteLanguage(@PathVariable Long id) {
+        if (languageService.getAllLanguages().get(id.intValue()) != null) {
+            deleteLanguage(id);
+        }
     }
 }
