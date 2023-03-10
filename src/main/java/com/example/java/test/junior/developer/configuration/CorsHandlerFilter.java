4a0661d73220f5a6e14b33e01248a4bd8348477a
rubusta <n.a.rubusta3@gmail.com>
2023-03-10 17:45:58 +0100

    roll back to Aleksandr commit

--- src/main/java/com/example/java/test/junior/developer/configuration/CorsHandlerFilter.java
+++ src/main/java/com/example/java/test/junior/developer/configuration/CorsHandlerFilter.java
@@ -42,7 +42,6 @@ public class CorsHandlerFilter extends OncePerRequestFilter {
         HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS,
         "accept,content-type,content-length,authorization");
     response.setStatus(HttpStatus.OK.value());
-
     return options && request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.name());
   }
 
