4afb9c293289c3813306a89515525a88598573be
Oleksandr Pogorelov <41591759+alex97po@users.noreply.github.com>
2023-03-12 12:33:11 +0400

    Connected service to RDS
    * Connected service to RDS

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -3,6 +3,11 @@
 # Change to the directory containing the JAR file
 cd /home/ec2-user/app
 
+# Fetch DB password from AWS SSM
+DB_PASSWORD=$(aws ssm get-parameters --region eu-central-1 --names DB_PASSWORD --with-decryption --query Parameters[0].Value)
+DB_PASSWORD=`echo $DB_PASSWORD | sed -e 's/^"//' -e 's/"$//'`
+export DB_PASSWORD=$DB_PASSWORD
+
 # Start the Java process in the background
 nohup java -jar java-test-junior-developer-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
 
