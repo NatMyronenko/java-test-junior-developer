6c8675562c60c9fec4c7cedcb79b99f460704ffe
rubusta <n.a.rubusta3@gmail.com>
2023-03-12 10:06:52 +0100

    added test controller

--- src/main/java/com/example/java/test/junior/developer/controller/CategoryController.java
+++ src/main/java/com/example/java/test/junior/developer/controller/CategoryController.java
@@ -18,6 +18,7 @@ public class CategoryController {
     public CategoryDto createCategory(@RequestBody @Valid CategoryDto category){
         return categoryService.createCategory(category);
     }
+
     @GetMapping
     public List<CategoryDto> getAllCategories() {
         return categoryService.getAllCategories();
