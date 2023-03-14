24d9d289addb6c1584d7dd566641fd5eb79c0343
Nataliia Myronenko <108218207+NatMyronenko@users.noreply.github.com>
2023-03-14 11:04:17 +0100

    Category crud endpoints (#31)

---
+++ src/main/java/com/example/java/test/junior/developer/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.example.java.test.junior.developer.mapper;
+
+import com.example.java.test.junior.developer.dto.CategoryDto;
+import com.example.java.test.junior.developer.model.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+    public Category toEntity(CategoryDto dto) {
+        return Category.builder()
+                .name(dto.getName())
+                .build();
+    }
+
+    public CategoryDto toDto(Category category) {
+        return CategoryDto.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
+    }
+}
+
--- src/test/java/com/example/java/test/junior/developer/language/LanguageControllerTest.java
+++ src/test/java/com/example/java/test/junior/developer/language/LanguageControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.java.test.junior.developer;
+package com.example.java.test.junior.developer.language;
 
 
 import static org.hamcrest.Matchers.equalTo;
