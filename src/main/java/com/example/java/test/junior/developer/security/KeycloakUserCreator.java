0e6ba2acae4d59f7764598d154e81c3f9cea47da
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-16 20:58:55 +0200

    created class KeycloakCreator

---
+++ src/main/java/com/example/java/test/junior/developer/security/KeycloakUserCreator.java
@@ -0,0 +1,50 @@
+package com.example.java.test.junior.developer.security;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+
+import java.util.Arrays;
+
+public class KeycloakUserCreator {
+    public void createUser() {
+        /* Параметри підключення до Keycloak сервера - створені для візуалізацї
+         *(будуть предаватись аргументами в білдер) Спробую їх імпортувати з пропертей
+         * використовуючи @Component та @Value
+         */
+        String serverUrl = "http://localhost:8080/auth";
+        String realm = "myrealm";
+        String clientId = "myclient";
+        String clientSecret = "mysecret";
+        String username = "myusername";
+        String password = "mypassword";
+
+        // Створення Keycloak клієнта
+        Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl(serverUrl)  // Ще не придумав як краще брати налаштування з properties
+                .realm(realm)          // те саме (налаштовується в Keycloak наскільки я зрозумів)
+                .clientId(clientId)    // можливо непотрібне поле =)
+                .clientSecret(clientSecret) // також з пропертя
+                .username(username)    // логін від Keycloak
+                .password(password)    // пароль від нього ж
+                .build();              // створити
+
+        // Створення user-а
+        UserRepresentation user = new UserRepresentation();
+        user.setFirstName("Aleks");         // В ці поля будуть підставлятись дані юзвірів
+        user.setLastName("aleksdenni");
+        user.setEmail("aleksdenni@example.com");
+        user.setEnabled(true);
+
+        // Встановлення пароля для user
+        CredentialRepresentation credential = new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setValue("password");
+        credential.setTemporary(false);
+        user.setCredentials(Arrays.asList(credential));
+
+        // Додавання user до Keycloak
+        keycloak.realm(realm).users().create(user);
+    }
+}
