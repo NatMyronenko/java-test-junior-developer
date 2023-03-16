28719625fea8e4013560f756638b3253fe2a68c9
rubusta <n.a.rubusta3@gmail.com>
2023-03-16 12:56:49 +0100

    added controllers,service,mapper TESTS

---
+++ src/test/java/com/example/java/test/junior/developer/question/QuestionMapperJunitTest.java
@@ -0,0 +1,45 @@
+package com.example.java.test.junior.developer.question;
+
+import com.example.java.test.junior.developer.dto.CategoryDto;
+import com.example.java.test.junior.developer.dto.QuestionDto;
+import com.example.java.test.junior.developer.mapper.CategoryMapper;
+import com.example.java.test.junior.developer.mapper.QuestionMapper;
+import com.example.java.test.junior.developer.model.Category;
+import com.example.java.test.junior.developer.model.Question;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class QuestionMapperJunitTest {
+
+    @InjectMocks
+    private QuestionMapper questionMapper;
+
+    @Test
+    public void toEntity_ValidDto_ReturnsEntity() {
+        QuestionDto questionDto = new QuestionDto(1, "What is polymorphism?");
+
+        Question result = questionMapper.toEntity(questionDto);
+
+        assertEquals(questionDto.getName(), result.getName());
+    }
+
+    @Test
+    public void toDto_ValidEntity_ReturnsDto() {
+        Question question = Question.builder()
+                .id(1)
+                .name("What is polymorphism?")
+                .build();
+
+        QuestionDto result = questionMapper.toDto(question);
+
+        assertEquals(question.getId(), result.getId());
+        assertEquals(question.getName(), result.getName());
+    }
+}
+
+
