f577a110259b96ff83a2e36cb35e9459724d5b92
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-18 11:56:17 +0200

    added field clientName and setter

--- src/main/java/com/example/java/test/junior/developer/security/KeycloakConfiguration.java
+++ src/main/java/com/example/java/test/junior/developer/security/KeycloakConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.java.test.junior.developer.security;
 
+import lombok.Setter;
 import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
@@ -7,19 +8,22 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //TODO: Розібратись з ConfigurationProperties (як діставати дані). username ?
+@Setter
 @Configuration
-@ConfigurationProperties(prefix = "keycloak")   // не зовсім розібрався як ця штука працює (атоматично підставляє?), витягнув дані з keycloakProperties
+// не зовсім розібрався як штука нижче працює (автоматично підставляє через setter?), витягнув дані з keycloakProperties
+@ConfigurationProperties(prefix = "keycloak")
 public class KeycloakConfiguration {
-    private KeycloakSpringBootProperties keycloakProperties;
+    private String clientName;
+    private KeycloakSpringBootProperties keycloakProperties; // Чи можна використати для введення даних?
 
     @Bean
     public Keycloak keycloak() {
         return KeycloakBuilder.builder()
                 .serverUrl(keycloakProperties.getAuthServerUrl())  // Ще не придумав як краще брати налаштування з properties
-                .realm(keycloakProperties.getRealm())          // те саме (налаштовується в Keycloak наскільки я зрозумів)
-                .clientId(keycloakProperties.getResource())    // можливо непотрібне поле =)
+                .realm(keycloakProperties.getRealm())              // те саме (налаштовується в Keycloak наскільки я зрозумів)
+                .clientId(keycloakProperties.getResource())        // можливо непотрібне поле =)
                // .clientSecret() // Думаю що це не потрібно
-                .username("admin")    // логін від Keycloak. Поки не знайшов де він вноситься
+                .username(clientName)    // логін від Keycloak. Поки не знайшов де він вноситься
                 .password(keycloakProperties.getClientKeyPassword())    // пароль від нього ж
                 .build();
     }
