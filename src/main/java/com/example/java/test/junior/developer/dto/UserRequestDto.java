ff51c6fef3d4c0b09a5419ab35e325f83d443959
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-24 23:44:56 +0200

    added a check

--- src/main/java/com/example/java/test/junior/developer/dto/UserRequestDto.java
+++ src/main/java/com/example/java/test/junior/developer/dto/UserRequestDto.java
@@ -14,15 +14,22 @@ import lombok.Value;
 public class UserRequestDto {
 
   @NotEmpty
-  @Pattern(regexp = "[a-zA-Zа-яА-Яіїє]+")
+  @Pattern(regexp = "^[a-zA-Z][-a-zA-Z\\s]{0,22}[a-zA-Z]$")
   @NonNull String firstName;
 
-  @Pattern(regexp = "[a-zA-Zа-яА-Яіїє]+")
+  @Pattern(regexp = "^[a-zA-Z][-a-zA-Z\\s]{0,22}[a-zA-Z]$")
   @NonNull String lastName;
 
-  @Email(regexp = "[\\S]+@[a-z0-9.\\-]+\\.[a-z]{2,6}", message = "Email should be valid")
+  @Email(regexp = "^(?=.{10,63}$)(?!.*\\s)[a-zA-Z0-9!#$%&'*+\\-=?^_`{|}~]"
+      + "+(?:[.\\-][a-zA-Z0-9!#$%&'*+\\-=?^_`{|}~]+)*"
+      + "@[a-z0-9](?:[a-z0-9\\-]*[a-z0-9])*"
+      + "(?:\\.[a-z0-9](?:[a-z0-9\\-]*[a-z0-9])*)+",
+      message = "Email should be valid")
   @NonNull String email;
 
   @NotEmpty
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)"
+      + "(?=.*[!\"#$%&'()*+,\\-./:;<=>?@[\\\\]^_{|}~])"
+      + "[A-Za-z\\d!\"#$%&'()*+,-./:;<=>?@[\\\\]^_`{|}~]{8,32}$" )
   @NonNull String password;
 }
