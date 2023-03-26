79b7798bdf367ba96114bc29ae5a4575c2d0591a
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-26 21:44:26 +0300

    fixed User style

--- src/main/java/com/example/java/test/junior/developer/model/User.java
+++ src/main/java/com/example/java/test/junior/developer/model/User.java
@@ -1,11 +1,15 @@
 package com.example.java.test.junior.developer.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Builder
 @Data
@@ -13,7 +17,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-public class User{
+public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
