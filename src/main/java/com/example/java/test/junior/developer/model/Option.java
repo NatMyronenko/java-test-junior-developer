aaebef3a990cb5b82083a865b1060e477192890f
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-29 23:54:25 +0300

    fixed Option

--- src/main/java/com/example/java/test/junior/developer/model/Option.java
+++ src/main/java/com/example/java/test/junior/developer/model/Option.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,8 +30,9 @@ public class Option {
   @Column(name = "answer", nullable = false)
   private String answer;
 
-  @Column(name = "question_id", nullable = false)
-  private String questionId;
+  @ManyToOne
+  @JoinColumn(name = "question_id", nullable = false)
+  private Question question;
 
   @Column(name = "correct", unique = true, nullable = false)
   private Boolean isCorrect;
