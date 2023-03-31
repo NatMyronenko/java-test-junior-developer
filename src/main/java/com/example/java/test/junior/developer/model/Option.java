8741a6e688fe6a32a6b75aa8cad70121c86acd8a
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-03-31 23:36:06 +0300

    fixed Option

--- src/main/java/com/example/java/test/junior/developer/model/Option.java
+++ src/main/java/com/example/java/test/junior/developer/model/Option.java
@@ -1,5 +1,6 @@
 package com.example.java.test.junior.developer.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,7 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,7 +30,7 @@ public class Option {
   @Column(name = "answer", nullable = false)
   private String answer;
 
-  @ManyToOne
+  @ManyToOne(cascade = CascadeType.REMOVE)
   @JoinColumn(name = "question_id", nullable = false)
   private Question question;
 
