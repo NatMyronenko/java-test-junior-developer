c1cef1fe34a776a1f02b1aecb922f00bdea7bf5f
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-04-06 14:14:50 +0300

    fixed Category

--- src/main/java/com/example/java/test/junior/developer/mapper/CategoryMapper.java
+++ src/main/java/com/example/java/test/junior/developer/mapper/CategoryMapper.java
@@ -2,13 +2,18 @@ package com.example.java.test.junior.developer.mapper;
 
 import com.example.java.test.junior.developer.dto.CategoryDto;
 import com.example.java.test.junior.developer.model.Category;
+import com.example.java.test.junior.developer.service.LanguageService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CategoryMapper {
+
+  LanguageService languageService;
+
   public Category toEntity(CategoryDto dto) {
     return Category.builder()
         .name(dto.getName())
+        .language(languageService.getLanguage(dto.getId()))
         .build();
   }
 
--- src/main/java/com/example/java/test/junior/developer/repository/CategoryRepository.java
+++ src/main/java/com/example/java/test/junior/developer/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.example.java.test.junior.developer.repository;
 
 import com.example.java.test.junior.developer.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
