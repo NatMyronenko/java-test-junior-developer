4baf32bfec7518fcaef740f15c189a461d83df9a
rubusta <n.a.rubusta3@gmail.com>
2023-04-16 12:22:00 +0200

    test cleaning

--- src/test/java/com/example/java/test/junior/developer/controller/LogOutControllerTest.java
+++ src/test/java/com/example/java/test/junior/developer/controller/LogOutControllerTest.java
@@ -3,17 +3,12 @@ package com.example.java.test.junior.developer.controller;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.java.test.junior.developer.dto.LogOutRequestDto;
-import com.example.java.test.junior.developer.dto.LoginRequestDto;
-import com.example.java.test.junior.developer.dto.LoginResponseDto;
 import com.example.java.test.junior.developer.service.AuthService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
