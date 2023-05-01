543366eca0eb98dbb222f3be5516e58c9945dcc2
rubusta <n.a.rubusta3@gmail.com>
2023-05-01 12:47:08 +0200

    for checkin

--- src/test/java/com/example/java/test/junior/developer/controller/AuthControllerTest.java
+++ src/test/java/com/example/java/test/junior/developer/controller/AuthControllerTest.java
@@ -9,16 +9,20 @@ import com.example.java.test.junior.developer.dto.LoginRequestDto;
 import com.example.java.test.junior.developer.dto.LoginResponseDto;
 import com.example.java.test.junior.developer.service.AuthService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
 @WebMvcTest(AuthController.class)
+@WithMockUser
 class AuthControllerTest {
 
   @Autowired
