d1edb66f93014a5dddccd1c535018d3dff60ef40
rubusta <n.a.rubusta3@gmail.com>
2023-05-07 21:02:25 +0200

    added keycloak settings for aws

--- src/test/java/com/example/java/test/junior/developer/JavaTestJuniorDeveloperApplicationTests.java
+++ src/test/java/com/example/java/test/junior/developer/JavaTestJuniorDeveloperApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.java.test.junior.developer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
