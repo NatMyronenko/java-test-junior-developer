99bee1e4856a643904a37f3c83fb9f63d145b572
Maksim Bilyi <w0957316192@gmail.com>
2023-05-14 22:35:23 +0300

    after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Download the application jar from S3
+# Download the application jar from S3 and backup
 aws s3 cp s3://java-junior-backend-artefacts/java-test-junior-developer-0.0.1-SNAPSHOT.jar /home/ec2-user/app/
