93a5f38f2e85b785404416f00c1158af37e61c3c
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-05-20 21:59:53 +0300

    add response status exception

--- src/main/java/com/example/java/test/junior/developer/keycloak/KeycloakAdminClient.java
+++ src/main/java/com/example/java/test/junior/developer/keycloak/KeycloakAdminClient.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
