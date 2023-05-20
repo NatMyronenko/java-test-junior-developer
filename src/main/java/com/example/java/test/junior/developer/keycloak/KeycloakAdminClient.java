be448264faacf7d30d4b161fde34099983dde20e
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-05-20 21:59:22 +0300

    add response status exception

--- src/main/java/com/example/java/test/junior/developer/keycloak/KeycloakAdminClient.java
+++ src/main/java/com/example/java/test/junior/developer/keycloak/KeycloakAdminClient.java
@@ -1,10 +1,14 @@
 package com.example.java.test.junior.developer.keycloak;
 
+import javax.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 @Slf4j
@@ -16,7 +20,17 @@ public class KeycloakAdminClient {
 
   public void createUser(UserRepresentation user) {
     log.info("Creating user with username: {}", user.getUsername());
-    keycloak.realm(keycloakProperties.getRealm()).users().create(user);
-    log.info("User created successfully");
+    var response = keycloak.realm(keycloakProperties.getRealm()).users().create(user);
+    int statusCode = response.getStatus();
+    if (statusCode == Response.Status.CREATED.getStatusCode()) {
+      log.info("User created successfully with status code: {}", statusCode);
+    } else {
+      log.error("User creation failed with status code: {}", statusCode);
+      try {
+        throw new RuntimeException("User creation failed");
+      } catch (RuntimeException e) {
+        throw new ResponseStatusException(HttpStatus.valueOf(statusCode));
+      }
+    }
   }
 }
